634ec8356da7d6c0fb54a749c68dd2b6f8a4c8bf
Jaeeun <jaeeun9862@gmail.com>
2023-02-06 12:20:01 +0900

    feat : useReducer, Context 설정

--- src/App.js
+++ src/App.js
@@ -1,57 +1,99 @@
 import React from "react";
+import { useReducer, useRef, createContext } from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./components/Home";
 import New from "./components/New";
 import Edit from "./components/Edit";
 import Diary from "./components/Diary";
 
-//BTN
-import MyButton from "./components/MyButton";
-import MyHeader from "./components/MyHeader";
-// import Menu from "./components/Menu";
+// reducer 함수
+const reducer = (state, action) => {
+  let newState = [];
+  switch (action.type) {
+    case "INIT": {
+      return action.data;
+    }
+    case "CREATE": {
+      newState = [...action.data, ...state];
+      break;
+    }
+    case "REMOVE": {
+      // 선택한 targetId를 제외한 배열로 업데이트
+      newState = state.filter((it) => it.id !== action.targetId);
+      break;
+    }
+    case "EDIT": {
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
+      break;
+    }
+    default: {
+      return state;
+    }
+  }
+  return newState;
+};
+
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 const App = () => {
-  const env = process.env;
-  env.PUBLIC_URL = env.PUBLIC_URL || "";
+  const [data, dispatch] = useReducer(reducer, []);
+  const dataId = useRef(0);
 
+  // Dispatch함수
+  // CREATE
+  const onCreate = (date, content, emotion) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: dataId.current,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+    dataId.current += 1;
+  };
+
+  // REMOVE
+  const onRemove = (targetId) => {
+    dispatch({
+      type: "REMOVE",
+      targetId: targetId,
+    });
+  };
+
+  // EDIT
+  const onEdit = (targetId, date, content, emotion) => {
+    dispatch({
+      type: "EDIT",
+      data: {
+        id: targetId,
+        date: new Date(date).getTime(),
+        content,
+        emotion,
+      },
+    });
+  };
   return (
-    <BrowserRouter>
-      <div className="App">
-        <MyHeader
-          headText={"헤더입니다"}
-          leftChild={
-            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
-          }
-          rightChild={
-            <MyButton
-              text={"오른쪽 버튼"}
-              onClick={() => alert("오른쪽 클릭")}
-            />
-          }
-        />
-        {/* <Menu /> */}
-        {/* <img src={process.env.PUBLIC_URL + "/assets/emotion1.png"} /> */}
-
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"positive"}
-        />
-        <MyButton
-          text={"버튼"}
-          onClick={() => alert("버튼 클릭")}
-          type={"negative"}
-        />
-        <MyButton text={"버튼"} onClick={() => alert("버튼 클릭")} />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/new" element={<New />} />
-          <Route path="/edit" element={<Edit />} />
-          <Route path="/diary/:id" element={<Diary />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <DiaryStateContext.Provider value={data}>
+      <DiaryDispatchContext.Provider value={(onCreate, onEdit, onRemove)}>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </DiaryDispatchContext.Provider>
+    </DiaryStateContext.Provider>
   );
 };
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <h2>Home</h2>
+      <div>이곳은 home 입니다</div>
+    </>
+  );
 };
 
 export default Home;
